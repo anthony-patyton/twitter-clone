1a69d845f69a32677ba8177f14664423a91b5f95
Anthony Payton <paytonanthony2020@gmail.com>
2021-09-22 15:00:25 -0600

    finished feed & tweetbox components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import Sidebar from './components/Sidebar';
+import Sidebar from './components/sidebar/Sidebar';
+import Feed from './components/feed/Feed';
+import Widgets from './components/widgets/Widgets';
 
 const  App = () => {
   return (
     <>
       <div className="app">
-        <h1>Hey Clever Programmers... Let's build a Twitter app</h1>
+        {/* <h1>Hey Clever Programmers... Let's build a Twitter app</h1> */}
 
         <Sidebar />
-
-        {/* Feed */}
-
-        {/* Widgets */}
+        <Feed />
+        <Widgets />
       </div>
     </>
   );
