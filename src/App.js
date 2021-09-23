a877aac73f23760cf56dea55f10491c57b17f387
Anthony Payton <paytonanthony2020@gmail.com>
2021-09-22 18:25:29 -0600

    styling done for the most part, only need posts

--- src/App.js
+++ src/App.js
@@ -5,15 +5,11 @@ import Widgets from './components/widgets/Widgets';
 
 const  App = () => {
   return (
-    <>
-      <div className="app">
-        {/* <h1>Hey Clever Programmers... Let's build a Twitter app</h1> */}
-
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      </div>
-    </>
+    <div className="app">
+      <Sidebar />
+      <Feed />
+      <Widgets />
+    </div>
   );
 }
 
